43b457aa39ebc93722afea05e50de637222b999e
Olena Poliarenko <opoliarenk@e1r6p1.unit.ua>
2021-03-15 19:01:52 +0200

    create db +

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize("usoftBackend", "root", "securepass", {
+const sequelize = new Sequelize("usoft", 'root', "smartucodelena", {
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
--- db/db.sql
+++
@@ -1,4 +0,0 @@
-create database usoftBackend;
-create user 'root'@'localhost' identified by 'securepass';
-GRANT ALL PRIVILEGES ON usoftBackend.* to 'root'@'localhost';
-FLUSH PRIVILEGES;
